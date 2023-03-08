23e6c20c36df6dad1f9283756f9d3cc8719cca8a
martin5169 <martincommisso@hotmail.com>
2023-03-08 11:35:59 -0300

    se arregla footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,13 +2,14 @@ import React from "react";
 
 function Footer() {
   return (
-    <div bgColor="light" className="text-center text-lg-left">
+    <div bgColor="light" class="fixed-bottom"  >
       <div
         className="text-center p-3"
         style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
       >
          {new Date().getFullYear()} - IMC
       </div>
+      
     </div>
   );
 }
